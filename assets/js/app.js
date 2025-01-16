97fdf1bcac5e1351987bcfdcad2a8b2191c1276e
ahsanarain <44175502+ahsanarain@users.noreply.github.com>
2025-01-16 15:57:40 +0500

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,18 @@
-const inputs = document.querySelectorAll(".input-field");
-const toggle_btn = document.querySelectorAll(".toggle");
-const main = document.querySelector("main");
-const bullets = document.querySelectorAll(".bullets span");
-const images = document.querySelectorAll(".image");
+const inputs = document.querySelectorAll(".input");
 
-inputs.forEach((inp) => {
-    inp.addEventListener("focus", () => {
-        inp.classList.add("active");
-    });
-    inp.addEventListener("blur", () => {
-        if (inp.value != "") return;
-        inp.classList.remove("active");
-    });
-});
-
-toggle_btn.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        main.classList.toggle("sign-up-mode");
-    });
-});
-
-function moveSlider() {
-    let index = this.dataset.value;
-
-    let currentImage = document.querySelector(`.img-${index}`);
-    images.forEach((img) => img.classList.remove("show"));
-    currentImage.classList.add("show");
-
-    const textSlider = document.querySelector(".text-group");
-    textSlider.style.transform = `translateY(${-(index - 1) * 2.2}rem)`;
-
-    bullets.forEach((bull) => bull.classList.remove("active"));
-    this.classList.add("active");
+function addcl(){
+    let parent = this.parentNode.parentNode;
+    parent.classList.add("focus");
 }
 
-bullets.forEach((bullet) => {
-    bullet.addEventListener("click", moveSlider);
+function remcl(){
+    let parent = this.parentNode.parentNode;
+    if(this.value == ""){
+        parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener("focus", addcl);
+    input.addEventListener("blur", remcl);
 });
