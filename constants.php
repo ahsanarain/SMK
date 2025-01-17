e541bdf7883a87b5454b306f9ec1d326f4d25bd3
ahsanarain <44175502+ahsanarain@users.noreply.github.com>
2025-01-17 13:03:05 +0500

    Add files via upload

---
+++ constants.php
@@ -0,0 +1,55 @@
+<?php
+$principal = 'PRINCIPAL';
+$principal_name = 'NAJMA SULTANA';
+$education = ' (MSN)';
+$principal_name_education = $principal_name . $education;
+
+$college = 'Qadri College of Health Sciences, Karachi';
+$ref = 'QCOHS/';
+$tablename = 'qcn';
+
+$image_location2 = 'width="75" height="75" style="margin-top:-90px;"';
+$image_location3 = "style='width:8%; height:8%; margin-left:47%'";
+$image_location5 = 'width="100" height="100" style="margin-top:-50px"';
+$image_location6 = 'width="75" height="100" ';
+$image_location7 = 'width="80" height="80" ';
+$image_location8 = 'width="40" height="40" ';
+
+
+$college_logo = '<img src="logo.png" width="80" height="80">';
+
+$qadri_college_logo = 'web_icon_2.png';
+
+
+$smf = "logo.png";
+
+
+$image_location4 = 'width="100" height="100"';
+
+$logo_name = "logo.png";
+
+
+$snb = 'nursing_board.jpg';
+
+
+$institute_head = "Ghulam Mujtaba Qadri";
+$institute_address = "B-128 Block-14 Gulistan-e-Johar, Karachi";
+$institute_email = "qadricohs@gmail.com";
+$phone = "03331237444";
+$coordinator = "Sir. Akhlaque Ahmed Qadri";
+$coordinator_no = "03331-2350149";
+
+$city = "Karachi";
+$district = "Karachi";
+$division = "Karachi";
+$province = "Sindh";
+
+// Bank Account
+
+$bank = "Meezan Bank LTD";
+$account_number = "99340106314091";
+$account_itle = "Qadri College of Health Sciences Karachi";
+
+$normal = "#485087";
+$dark = "#2f376e";
+$light = "#5d6495";
